6bb72a65c635022ccacf5e3195b4dcc996db0f53
ntx745120 <ntx745120@163.com>
2019-10-14 11:31:22 +0800

    no message

--- bin/index.js
+++ bin/index.js
@@ -9,7 +9,7 @@ window.screenOrientation = "sensor_landscape";
 
 //-----libs-begin-----
 loadLib("libs/laya.core.js")
+loadLib("libs/laya.ani.js")
 loadLib("libs/laya.ui.js")
-loadLib("libs/laya.physics.js")
 //-----libs-end-------
 loadLib("js/bundle.js");
--- src/GameConfig.ts
+++ src/GameConfig.ts
@@ -4,16 +4,16 @@
 * 游戏初始化配置;
 */
 export default class GameConfig{
-    static width:number=640;
-    static height:number=1136;
+    static width:number=750;
+    static height:number=1334;
     static scaleMode:string="fixedwidth";
     static screenMode:string="none";
     static alignV:string="top";
     static alignH:string="left";
-    static startScene:any="";
+    static startScene:any="mainui.scene";
     static sceneRoot:string="";
     static debug:boolean=false;
-    static stat:boolean=false;
+    static stat:boolean=true;
     static physicsDebug:boolean=false;
     static exportSceneToJson:boolean=true;
     constructor(){}
